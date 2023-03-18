d5ed7f9349d7561681f48714b8f9e2623f223ef2
AysuDemirci <aysu.gunerr@gmail.com>
2023-03-18 13:25:16 +0300

    added add/remove button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Container } from "reactstrap";
+import QuestionTag from "./Components/QuestionTag";
 
 export default function App() {
   return (
     <div>
-     
+      <div className="header">
+        <h3>Create a question tag</h3>
+      </div>
+      <br />
+      <Container className="body">
+        <QuestionTag />
+      </Container>
     </div>
   );
 }
-
-
