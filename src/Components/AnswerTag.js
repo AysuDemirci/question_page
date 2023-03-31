2846c8a5f7f505d0b134558cfd0e4dc70ead5d43
AysuDemirci <aysu.gunerr@gmail.com>
2023-03-31 22:21:42 +0300

    added hamburger sidenavbar

--- src/Components/AnswerTag.js
+++ src/Components/AnswerTag.js
@@ -27,29 +27,44 @@ export default function AnswerTag() {
         return (
           <div>
             <Row>
-              <Input
-                style={{ width: "400px", marginBottom: "15px",marginLeft:"13px" }}
-                name="question"
-                placeholder="Enter Answer"
-                onChange={(e) => handleAnswerChange(e, i)}
-                value={x.answer}
-              />
-              <div>
-                {answerList.length !== 1 && (
-                  <Button onClick={() => handleRemoveClick(i)}>Remove</Button>
-                )}
-                {answerList.length - 1 === i && (
-                  <Button
-                    className="answer-btn"
-                    onClick={() => handleAddClick(i)}
-                  >
-                    Add
-                  </Button>
-                )}{" "}
-              </div>
-            </Row>
+              <ul style={{ display: "flex", gap: "20px", listStyle: "none" }}>
+                <li>
+                  <Input
+                    style={{
+                      width: "400px",
+                      marginBottom: "15px",
+                      marginLeft: "13px",
+                    }}
+                    name="answer"
+                    placeholder="Enter Answer"
+                    onChange={(e) => handleAnswerChange(e, i)}
+                    value={x.answer}
+                  />
+                </li>
 
-            {JSON.stringify(answerList)}
+                <li>
+                  {answerList.length - 1 === i && (
+                    <Button
+                      className="answer-btn2"
+                      onClick={handleAddClick}
+                    >
+                      Add
+                    </Button>
+                  )}{" "}
+                </li>
+
+                <li>
+                  {answerList.length !== 1 && (
+                    <Button
+                      onClick={() => handleRemoveClick(i)}
+                      className="answer-btn"
+                    >
+                      Remove
+                    </Button>
+                  )}
+                </li>
+              </ul>
+            </Row>
           </div>
         );
       })}
--- src/Components/QuestionTag.js
+++ src/Components/QuestionTag.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Input, Row, Col } from "reactstrap";
+import {  Input, Row, Col, Container } from "reactstrap";
 import AnswerTag from "./AnswerTag";
 
 export default function QuestionTag() {
@@ -23,47 +23,77 @@ export default function QuestionTag() {
   };
 
   return (
-    <div>
+    <Container style={{}}>
+      <div>
       <br />
+      <br />
+      
       <h5>If you want to add more questions, please click Add button</h5>
       <br />
       {inputList.map((x, i) => {
         return (
           <div>
-            <Col md="6">
-             
-              <Row>
-                <Input
-                  style={{ width: "400px", marginBottom: "15px",marginLeft:"13px" }}
-                  name="question"
-                  placeholder="Enter Question"
-                  onChange={(e) => handleInputChange(e, i)}
-                  value={x.question}
-                />
-                <div>
-                  <AnswerTag/>
+            <Col md="6" style={{ marginLeft: "20px" }}>
+              <ul
+                style={{
+                  display: "flex",
+                  gap: "10px",
+                  listStyle: "none",
+                  marginLeft: "500px",
+                }}
+              >
+                <li>
+                  {" "}
+                  {inputList.length - 1 === i && (
+                    <button
+                      className="question-btn2"
+                      onClick={handleAddClick}
+                    
+                    >
+                      Add
+                    </button>
+                  )}
+                </li>
+                <li>
                   {inputList.length !== 1 && (
-                    <Button
+                    <button
                       className="question-btn"
                       onClick={() => handleRemoveClick(i)}
+                    
                     >
                       Remove
-                    </Button>
+                    </button>
                   )}
-                  {inputList.length - 1 === i && (
-                    <Button className="question-btn2" onClick={handleAddClick}>
-                      Add
-                    </Button>
-                  )}
-                </div>
-                
+                </li>
+              </ul>
+
+              <Row className="question-row-style">
+                <ul style={{ listStyle: "none" }}>
+                  <li>
+                    <Input
+                      style={{
+                        width: "400px",
+                        marginBottom: "15px",
+                        marginLeft: "13px",
+                      }}
+                      name="question"
+                      placeholder="Enter Question"
+                      onChange={(e) => handleInputChange(e, i)}
+                      value={x.question}
+                    />
+                  </li>
+                </ul>
+                <AnswerTag />
               </Row>
+              <br />
             </Col>
           </div>
         );
       })}
-      
+
       <br />
     </div>
+    </Container>
+    
   );
 }
