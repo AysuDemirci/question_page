250caf6de136a57e65f219b2539f2698991b8e01
AysuDemirci <aysu.gunerr@gmail.com>
2023-04-10 22:07:18 +0300

    added letter before answer inputs

--- src/Components/SideNavbar.js
+++ src/Components/SideNavbar.js
@@ -14,7 +14,6 @@ import { GrCircleInformation, GrContact } from "react-icons/gr";
 export default function SideNavbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [style, setStyle] = useState();
-  
 
   function toggle() {
     if (!isMenuOpen) {
@@ -24,7 +23,7 @@ export default function SideNavbar() {
         backgroundColor: "#ededed",
         height: "1065px",
         position: "fixed",
-        marginTop:"0px"
+        marginTop: "0px",
       });
     } else {
       setIsMenuOpen(false);
@@ -34,8 +33,6 @@ export default function SideNavbar() {
     }
   }
 
- 
-
   return (
     <div style={style}>
       <Button onClick={toggle} className="sidenav-btn">
@@ -44,45 +41,57 @@ export default function SideNavbar() {
       {isMenuOpen && (
         <div>
           <ul style={{ marginTop: "50px" }}>
-            <li className="menu-items-style animation-li">
-              <FaHome className="icons" />
-              Home
+            <li className="animation-li">
+              <a href="/" className="ahref">
+                <FaHome className="icons" />
+                Home
+              </a>
             </li>
 
             <br />
-            <li className="menu-items-style1 animation-li">
-              <GrCircleInformation className="icons2" />
-              Information
+            <li className="animation-li">
+              <a href="/" className="ahref">
+                <GrCircleInformation className="icons2" />
+                Information
+              </a>
             </li>
             <br />
-            <li className="menu-items-style2 animation-li">
-              <GrContact className="icons3" />
-              Contact Us
+            <li className="animation-li">
+              <a href="/" className="ahref">
+                {" "}
+                <GrContact className="icons3" />
+                Contact Us
+              </a>
             </li>
-            
           </ul>
-          
-          <div style={{marginTop:"430px"}}>
-           <h3 style={{marginLeft:"95px",marginBottom:"25px",fontFamily:"Delicious Handrawn, cursive"}}>Follow Us!</h3>
+
+          <div style={{ marginTop: "430px" }}>
+            <h3
+              style={{
+                marginLeft: "95px",
+                marginBottom: "25px",
+                fontFamily: "Delicious Handrawn, cursive",
+              }}
+            >
+              Follow Us!
+            </h3>
             <ul style={{ display: "flex", listStyle: "none", gap: "25px" }}>
               <li>
                 <FaFacebook className="follow-icons" />
               </li>
               <li>
-                <FaInstagramSquare className="follow-icons"/>
+                <FaInstagramSquare className="follow-icons" />
               </li>
               <li>
-                <FaTwitter className="follow-icons"/>
+                <FaTwitter className="follow-icons" />
               </li>
               <li>
-                <FaLinkedin className="follow-icons"/>
+                <FaLinkedin className="follow-icons" />
               </li>
             </ul>
           </div>
         </div>
       )}
     </div>
-
-   
   );
 }
